3c9eb14476ee57173b58803814882c09e47313dc
PeymonA <peymon.armand4@gmail.com>
2025-09-19 20:47:55 +1200

    added clustering of pins

--- frontend/src/components/PoiMarkers.jsx
+++ frontend/src/components/PoiMarkers.jsx
@@ -1,16 +1,54 @@
 import {
   AdvancedMarker,
-  Pin
+  Pin,
+  useMap
 } from '@vis.gl/react-google-maps';
 
-const PoiMarkers = (props ) => {
+import {MarkerClusterer} from '@googlemaps/markerclusterer';
+
+import React, {useEffect, useRef, useState} from 'react';
+
+const PoiMarkers = (props) => {
+  const map = useMap();
+  const [markers, setMarkers] = useState({});
+  const clusterer = useRef(null);
+
+  useEffect(() => {
+    if (!map) return;
+    if (!clusterer.current) {
+      clusterer.current = new MarkerClusterer({map});
+    }
+  }, [map]);
+
+  useEffect(() => {
+    clusterer.current?.clearMarkers();
+    clusterer.current?.addMarkers(Object.values(markers));
+  }, [markers]);
+
+  const setMarkerRef = (marker, key) => {
+    if (marker && markers[key]) return;
+    if (!marker && !markers[key]) return;
+
+    setMarkers(prev => {
+      if (marker) {
+        return {...prev, [key]: marker};
+      } else {
+        const newMarkers = {...prev};
+        delete newMarkers[key];
+        return newMarkers;
+      }
+    });
+  };
+
   return (
     <>
       {props.pois.map( (poi) => (
         <AdvancedMarker
           key={poi.key}
-          position={poi.location}>
-          <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
+          position={poi.location}
+          ref={marker => setMarkerRef(marker, poi.key)}
+          >
+            <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
         </AdvancedMarker>
       ))}
     </>
